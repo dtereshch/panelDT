23da320de5569b14bbe083b72ca4f2d26c61c66b
Dmitrii Tereshchenko <dtereshch@gmail.com>
2025-11-14 16:04:19 +0300

    Update example_EmplUK.R

--- example_EmplUK.R
+++ example_EmplUK.R
@@ -1,5 +1,44 @@
-library(devtools)
-install_github("dtereshch/panelDT")
+# Example
+# panelDT functions usage with plm::EmplUK dataset
+
+# Setup ========================================================================================
+
+# library(devtools)
+# install_github("dtereshch/panelDT")
 
 library(plm)
+library(fixest)
 library(panelDT)
+
+
+# Load & prepare the data =======================================================================
+
+data(EmplUK)
+is.pbalanced(EmplUK)
+
+df <- make.pbalanced(EmplUK, balance.type = "fill")
+
+df_plm <- pdata.frame(df, index = c("firm", "year"))
+df_fixest <- panel(df, panel.id = ~ firm + year)
+
+class(df)
+class(df_plm)
+class(df_fixest)
+
+
+# Explore the data ==============================================================================
+
+## General case ---------------------------------------------------------------------------------
+
+### NAs
+
+find_incomplete(df, group = "firm")
+explore_incomplete(df, group = "firm")
+
+describe_participation(df, group = "firm", time = "year")
+explore_participation(df, group = "firm", time = "year")
+plot_participation(df, group = "firm", time = "year")
+
+## plm::pdata.frame() class ---------------------------------------------------------------------
+
+## fixest::panel() class ------------------------------------------------------------------------
